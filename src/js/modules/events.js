ce51a2cef91a3f80df30ea7b9469f785c0bf38ad
Cuber00 <snidak2018@yandex.ru>
2022-04-30 07:56:44 +0700

    переработал js показ скрытия меню и других элементов

--- src/js/modules/events.js
+++ src/js/modules/events.js
@@ -1,32 +1,72 @@
 export function addEvents() {
     eventeMenu();
-
+    eventToFilter();
+    eventToFilterChecbox();
 }
 
-function eventeMenu(){
-    //Вешаем событие на показ и скрытие меню
+function eventToFilterChecbox(){
+    const productNav = document.querySelector(".products-nav");
+    if(!productNav) return;
 
+    const productNavBtn = productNav.querySelectorAll(".products-nav__btn");
+    
+    productNavBtn.forEach(item => {
+        item.addEventListener("click", () => {
+            const ul = item.nextElementSibling;
+            const toggleUl = () => ul.classList.toggle("products-nav__list--active");
+
+            toggleUl();
+            toggleEvent(ul, item, "products-nav__list--active", toggleUl);
+        })
+    })
+}
+
+function eventToFilter(){
+    const wrapFilter = document.querySelector(".filter-wrap");
+    
+    if(!wrapFilter) return;
+    
+
+    const btn = wrapFilter.querySelector(".filter-btn");
+    const filterItem = wrapFilter.querySelectorAll(".filter__item-title");
+    const toggleFilter = () => wrapFilter.classList.toggle("filter--active");
+
+    btn.addEventListener("click", () => toggleFilter);
+
+    filterItem.forEach(item => {
+        item.addEventListener("click", () => item.classList.toggle("filter__item-title--active"));
+    })
+    toggleEvent(wrapFilter, btn, "filter--active", toggleFilter);
+}
+
+
+
+function eventeMenu(){
     const burger = document.querySelector('.header__burger-icon');
     const menu = document.querySelector('.menu');
-    
-    const toggleMenu = () => {
-        menu.classList.toggle('menu--active');
-    }
+
+    const toggleMenu = () => menu.classList.toggle('menu--active');
     
     burger.addEventListener('click', e => {
         e.stopPropagation();
         toggleMenu();
     });
 
+    toggleEvent(menu, burger, "menu--active", toggleMenu);
+}
+
+function toggleEvent(el, btn, cls, toggle){
+    
     document.addEventListener('click', e => {
         const target = e.target;
-        const its_menu = target == menu || menu.contains(target);
-        const its_hamburger = target == burger;
-        const menu_is_active = menu.classList.contains('menu--active');
-        
-        if (!its_menu && !its_hamburger && menu_is_active) {
-          toggleMenu();
+        const its_menu = target == el || el.contains(target);
+        const its_hamburger = target == btn;
+        const menu_is_active = el.classList.contains(cls);
+
+        if(!its_menu && !its_hamburger && menu_is_active) {
+            toggle();
+        } else if (its_menu && its_hamburger && !menu_is_active) {
+            toggle();
         }
     })
 }
-
